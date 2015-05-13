eae35eb11da145928e82fcfc18cb0f88a52c5f38
otakuto <otakuto5648@gmail.com>
2015-05-13 15:56:22 +0900

    add fullscreen mode

--- main.cpp
+++ main.cpp
@@ -62,18 +62,15 @@ int main(int argc, char * argv[])
 	RubiksCube<SIZE> rc;
 
 	glfwInit();
-	//glfwOpenWindow(0, 0, 0, 0, 0, 0, 0, 0, GLFW_WINDOW);
-    GLFWwindow * window = glfwCreateWindow(640, 200, "0xFFFF", nullptr, nullptr);
-
-	/*
-	glfwSetWindowSizeCallback([](int w, int h)
-	{
-		glViewport(0, 0, w, h);
-		glLoadIdentity();
-		gluPerspective(90, static_cast<double>(w) / static_cast<double>(h), 1, 128);
-		gluLookAt(10, 10, 10, 0, 0, 0, 0, 1, 0);
-	});
-	*/
+	int count;
+	GLFWmonitor ** monitors = glfwGetMonitors(&count);
+	GLFWmonitor * monitor = monitors[1];//glfwGetPrimaryMonitor();
+	GLFWvidmode const * mode = glfwGetVideoMode(monitor);
+	glfwWindowHint(GLFW_RED_BITS, mode->redBits);
+	glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
+	glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
+	glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
+	GLFWwindow * window = glfwCreateWindow(mode->width, mode->height, "RubiksCube", monitor, nullptr);
     glfwMakeContextCurrent(window);
 
 	/*auto pa = [&](std::string s)
@@ -89,13 +86,13 @@ int main(int argc, char * argv[])
 	};*/
 
     while(!glfwWindowShouldClose(window))
-	//while (glfwGetWindowParam(GLFW_OPENED))
 	{
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glEnable(GL_DEPTH_TEST);
 		glPopMatrix();
 		glPushMatrix();
 
+		//window size chagne
 		int width, height;
 		glfwGetFramebufferSize(window, &width, &height);
 		glViewport(0, 0, width, height);
